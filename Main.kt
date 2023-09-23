52f81ac235c0d18722271c751892e8c23741eee7
DGutierrez81 <dgutierrezbueno81@gmail.com>
2023-09-23 18:25:31 +0200

    archivo jar principal

---
+++ Main.kt
@@ -0,0 +1,42 @@
+import java.io.*
+
+fun main(){
+
+    val archivo = File("itinerarios.csv")
+
+    val buffer = archivo.bufferedReader()
+
+    val texto = buffer.readLines()
+
+    val parametros = texto[0].split(",")
+
+    var parametro = ""
+
+    var campo = ""
+    
+
+    for(i in parametros){
+        if(i.equals("RECURSO_NOMBRE")){
+            parametro = i
+        }
+    }
+
+    val lista2 = mutableListOf<List<String>>()
+
+
+
+    for(s in 1..texto.size - 1){
+        lista2.add(s - 1, texto[s].split(","))
+    }
+
+    for(l in lista2){
+        for(p in l){
+            if(p.equals("Mercado municipal de Vila Real de Santo Antonio")){
+                campo = p
+            }
+        }
+    }
+
+    println(parametro)
+    println(campo)
+}
